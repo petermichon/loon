32d5251445e337253e61d9c7e107c29424c18ac1
shrln <shrln0@proton.me>
2024-11-30 16:36:11 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+import { parse } from "./parser.ts";
+
 export default function main() {
-    // ...
+    const fileAsString = Deno.readTextFileSync("./src/data/a_example.in");
+
+    const fileContent = parse(fileAsString);
+
+    console.log(fileContent);
 }
