0132b2b47e7abe834c4c9d1e8feeccedcc98ed9a
shrln <shrln0@proton.me>
2024-12-01 18:24:35 +0100

    Working altitude layer generation

--- src/network.ts
+++ src/network.ts
@@ -25,7 +25,7 @@ export function network() {
     {
         // The content of the input data file
         const input = {
-            grid: { rows: 1, columns: 3, altitudes: 1 },
+            grid: { rows: 2, columns: 3, altitudes: 2 },
             elements: { targets: 1, radius: 1, balloons: 1, turns: 1 },
             start: { row: 0, column: 0 },
             targets: [{ row: 0, column: 1 }],
@@ -51,45 +51,54 @@ export function network() {
 
     {
         type Layer = number[][];
-        const nbNodesPerLayer = R * C * (A + 1);
+
+        const nbAltitudes = A + 1;
         const nbLayers = T * 2;
+        const nbNodesPerLayer = R * C * nbAltitudes;
 
-        const network: Layer[] = new Array(nbLayers);
+        const network: Layer[] = [];
 
-        for (let iLayer = 0; iLayer < 2; iLayer++) {
-            network[iLayer] = new Array(nbNodesPerLayer);
+        for (let iLayer = 0; iLayer < nbLayers; iLayer++) {
+            network[iLayer] = [];
 
             const isAltitudeLayer = iLayer % 2 == 0;
             const isWindLayer = iLayer % 2 == 1;
 
             for (let iNode = 0; iNode < nbNodesPerLayer; iNode++) {
-                network[iLayer][iNode] = new Array(nbNodesPerLayer);
+                network[iLayer][iNode] = [];
+
                 for (let iEdge = 0; iEdge < nbNodesPerLayer; iEdge++) {
                     network[iLayer][iNode][iEdge] = 0;
 
                     if (isAltitudeLayer) {
-                        const isSameCell = iNode % C == iEdge % C;
+                        // The distance in a layer between two nodes that represents the same cell but at different altitudes
+                        const gap = C * R;
+
+                        // True if the edge is between two cells that represents the same cell, at == or != altitudes
+                        const isSameCell = iNode % gap == iEdge % gap;
+
                         if (isSameCell) {
-                            if (iNode < C) {
+                            // True if the edge is close enough for an altitude change, and not at altitude 0 (ground)
+                            const inLowerRange = iNode <= iEdge + gap;
+                            const inUpperRange = iNode >= iEdge - gap;
+                            const inRange = inLowerRange && inUpperRange;
+                            const isNotGround = iNode < gap || gap <= iEdge;
+
+                            if (inRange && isNotGround) {
                                 network[iLayer][iNode][iEdge] = 1;
-                            }
-                            if (iNode >= C) {
-                                if (iEdge > 2) {
-                                    network[iLayer][iNode][iEdge] = 1;
-                                }
                             }
                         }
                     }
 
                     if (isWindLayer) {
-                        if (iNode < 3) {
+                        if (iNode < C) {
                             network[iLayer][iNode][iNode] = 1;
                         }
-                        if (iNode >= 3) {
+                        if (iNode >= C) {
                             const dR = winds[0][0][0].r;
                             const dC = winds[0][0][0].c;
-                            const nR = ((iNode + dR) % C) + 3;
-                            const nC = ((iNode + dC) % C) + 3;
+                            const nR = ((iNode + dR) % C) + C;
+                            const nC = ((iNode + dC) % C) + C;
                             network[iLayer][nR][nC] = 1;
                         }
                     }
@@ -97,6 +106,17 @@ export function network() {
             }
         }
 
-        console.log(network);
+        // DEBUG
+        for (let iLayer = 0; iLayer < network.length; iLayer++) {
+            for (let iNode = 0; iNode < nbNodesPerLayer; iNode++) {
+                console.log(
+                    network[iLayer][iNode]
+                        .toString()
+                        .replace(/,/g, " ")
+                        .replace(/0/g, "·")
+                );
+            }
+            console.log();
+        }
     }
 }
