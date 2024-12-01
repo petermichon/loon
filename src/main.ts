5926779d173232ba69e6db1a1245ea57035ffcd0
shrln <shrln0@proton.me>
2024-12-01 01:51:03 +0100

    Rename data file

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { parse } from "./parser.ts";
 
 export default function main() {
-    const fileAsString = Deno.readTextFileSync("./src/data/a_example.in");
+    const fileAsString = Deno.readTextFileSync("./src/input/a_example.in");
 
     const fileContent = parse(fileAsString);
 
