d0abb79c28bc84925b4b00c03b4c42679841a3a0
shrln <shrln0@proton.me>
2024-12-01 14:02:25 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
-import { extractContent } from "./extract.ts";
-import { formatContent } from "./format.ts";
+// import { extractContent } from "./extract.ts";
+import { network } from "./network.ts";
+// import { formatContent } from "./format.ts";
 
 export default function main() {
-    const fileAsString = Deno.readTextFileSync("./src/input/a_example.in");
+    // const fileAsString = Deno.readTextFileSync("./src/input/a_example.in");
 
-    const fileContent = extractContent(fileAsString);
+    // const fileContent = extractContent(fileAsString);
 
-    console.log(fileContent);
+    // console.log(fileContent);
 
-    // ...
+    network();
 
-    const altitudeAdjustments = [1, 1, 1, 0, 0];
+    // const altitudeAdjustments = [1, 1, 1, 0, 0];
 
-    const outputContent = formatContent(altitudeAdjustments);
+    // const outputContent = formatContent(altitudeAdjustments);
 
-    Deno.writeTextFileSync("./src/output/a_example.out", outputContent);
+    // Deno.writeTextFileSync("./src/output/a_example.out", outputContent);
 }
